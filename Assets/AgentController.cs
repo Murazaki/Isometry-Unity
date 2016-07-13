5aad1322daa7bc068bbd9333f2f41191fe9b76a3
Mehdi Benadel <mehdi.benadel@gmail.com>
2016-07-13 10:45:29 +0200

    an empty tree sprouts first code and meditation git commit --all, push﻿

---
+++ Assets/AgentController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class AgentController : MonoBehaviour {
+	private NavMeshAgent agent;
+	private GameObject pointer;
+	
+	public GameObject pointerPrefab;
+	public Camera mainCamera;
+	
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+		pointer = (GameObject)Instantiate(pointerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+    void Update() {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0)) {
+			Debug.Log("Mouse button pushed !");
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 1000f, LayerMask.NameToLayer("Walls"))) {
+				Debug.Log("Raycast Hit !");
+				if(hit.collider.CompareTag("Terrain")) {
+					if(pointer) {
+						pointer.transform.position = hit.point;
+					}
+					
+					agent.SetDestination(hit.point);
+				}
+			}
+        }
+    }
+}
---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	public GameObject player;
+	private Vector3 playerViewportPos;
+	
+	public Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerViewportPos = mainCamera.WorldToViewportPoint(player.transform.position);
+		
+		Debug.Log("playerViewportPos : " + playerViewportPos);
+		
+		
+		
+		transform.localPosition = Vector3.Scale((playerViewportPos - 0.5f * Vector3.one), new Vector3(30f, 20f, 0f))
+							+ Vector3.Project(transform.localPosition, Vector3.forward);
+							
+		Debug.Log("See Through Camera Pos : " + transform.localPosition);
+	}
+}
